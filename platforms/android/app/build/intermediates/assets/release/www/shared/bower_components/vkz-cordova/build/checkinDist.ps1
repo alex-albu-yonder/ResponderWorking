cdbd7390ad5211cbd470f3c6d004c67801b36cf7
DevMac <devmac@DevMac.local>
2019-04-23 14:14:30 +0200

    Remove(gitignore-files): gitignore files that were accidentally include somehow

--- platforms/android/app/build/intermediates/assets/release/www/shared/bower_components/vkz-cordova/build/checkinDist.ps1
+++
@@ -1,4 +0,0 @@
-git add .\dist 2>&1 | write-host
-git status 2>&1 | write-host
-git commit -m "***NO_CI*** release checkin" 2>&1 | write-host
-git push 2>&1 | write-host
--- www/shared/bower_components/vkz-cordova/build/checkoutBranch.ps1
+++
@@ -1,2 +0,0 @@
-git checkout $env:BUILD_SOURCEBRANCHNAME 2>&1 | write-host
-Write-Host $LastExitCode
